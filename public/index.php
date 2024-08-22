2092a2fab6c2fd9cfe8db8282acf3ccb1f8e0eae
erdaserhan <erhan209@gmail.com>
2024-08-22 23:32:46 +0200

    index.php updated

--- public/index.php
+++ public/index.php
@@ -12,3 +12,26 @@ use model\OurPDO;
 
 // Appel de la config
 require_once "../config.php";
+
+// Notre autoload de classe
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', '/', $class);
+    require PROJECT_DIRECTORY.'/' .$class . '.php';
+});
+
+// chargement de l'autoload de composer
+require_once PROJECT_DIRECTORY.'/vendor/autoload.php';
+
+// chemin vers les templates twig
+$loader = new FilesystemLoader(PROJECT_DIRECTORY.'/view/');
+// création d'une instance de $twig
+$twig = new Environment($loader, [
+    'cache' => false, // pas de cache en dev
+    // 'cache' => '/path/to/compilation_cache', // chemin du cache pour la prod
+    // activation du debug en dev
+    'debug' => true,
+]);
+
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+$db = MyPDO::getInstance();
